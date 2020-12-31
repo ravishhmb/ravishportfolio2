15d7d1a6a73883fbfa1332c69a8f0fcca75be537
ravishhmb <76579907+ravishhmb@users.noreply.github.com>
2020-12-31 18:50:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,3 +10,26 @@ btn.addEventListener("click", ()=>{
         header.style.top = "-140px";
     }
 });
+
+const texts = ['Namaste,','Hello,','Welcome,'];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+(function type(){
+
+    if(count === texts.length){
+        count = 0;
+    }
+    currentText = texts[count];
+    letter = currentText.slice(0, ++index);
+    
+    document.querySelector('.namaste').textContent = letter;
+    if(letter.length === currentText.length){
+        count++;
+        index = 0;
+    }
+    setTimeout(type, 300);
+
+}());
